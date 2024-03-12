c9eb2408127de1cbe828f09d19272cdba032c953
Yohan Mayorga <yohanucv@gmail.com>
2024-03-12 14:27:14 -0400

    background added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@ import styles from "./page.module.css";
 import { getNeo } from "@/lib/Nasa";
 import Table from "@/components/table";
 import logo from "@/../../public/assets/logo.png";
+import Stars from "@/components/stars";
 
 export default function Home() {
   return (
     <div className={styles.body}>
+      <Stars />
       <Image src={logo} alt="logo" className={styles.logo} priority />
       <Table />
     </div>
---
+++ src/components/stars.tsx
@@ -0,0 +1,98 @@
+import styles from "@/styles/Stars.module.css";
+
+export default function Stars() {
+  return (
+    <div className={styles.body}>
+      <span
+        className={styles.star}
+        style={{
+          top: 0,
+          left: Math.floor(Math.random() * (100 - -100) + -100) + "%",
+          animationDelay: Math.random() * (0.8 - 0.2) + 0.2 + "s",
+          animationDuration: Math.floor(Math.random() * (3 - 2) + 2) + "s",
+        }}
+      ></span>
+      <span
+        className={styles.star}
+        style={{
+          top: 0,
+          left: Math.floor(Math.random() * (100 - -100) + -100) + "%",
+          animationDelay: Math.random() * (0.8 - 0.2) + 0.2 + "s",
+          animationDuration: Math.floor(Math.random() * (3 - 2) + 2) + "s",
+        }}
+      ></span>
+      <span
+        className={styles.star}
+        style={{
+          top: 0,
+          left: Math.floor(Math.random() * (100 - -100) + -100) + "%",
+          animationDelay: Math.random() * (0.8 - 0.2) + 0.2 + "s",
+          animationDuration: Math.floor(Math.random() * (3 - 2) + 2) + "s",
+        }}
+      ></span>
+      <span
+        className={styles.star}
+        style={{
+          top: 0,
+          left: Math.floor(Math.random() * (100 - -100) + -100) + "%",
+          animationDelay: Math.random() * (0.8 - 0.2) + 0.2 + "s",
+          animationDuration: Math.floor(Math.random() * (3 - 2) + 2) + "s",
+        }}
+      ></span>
+      <span
+        className={styles.star}
+        style={{
+          top: 0,
+          left: Math.floor(Math.random() * (100 - -100) + -100) + "%",
+          animationDelay: Math.random() * (0.8 - 0.2) + 0.2 + "s",
+          animationDuration: Math.floor(Math.random() * (3 - 2) + 2) + "s",
+        }}
+      ></span>
+      <span
+        className={styles.star}
+        style={{
+          top: 0,
+          left: Math.floor(Math.random() * (100 - -100) + -100) + "%",
+          animationDelay: Math.random() * (0.8 - 0.2) + 0.2 + "s",
+          animationDuration: Math.floor(Math.random() * (3 - 2) + 2) + "s",
+        }}
+      ></span>
+      <span
+        className={styles.star}
+        style={{
+          top: 0,
+          left: Math.floor(Math.random() * (100 - -100) + -100) + "%",
+          animationDelay: Math.random() * (0.8 - 0.2) + 0.2 + "s",
+          animationDuration: Math.floor(Math.random() * (3 - 2) + 2) + "s",
+        }}
+      ></span>
+      <span
+        className={styles.star}
+        style={{
+          top: 0,
+          left: Math.floor(Math.random() * (100 - -100) + -100) + "%",
+          animationDelay: Math.random() * (0.8 - 0.2) + 0.2 + "s",
+          animationDuration: Math.floor(Math.random() * (3 - 2) + 2) + "s",
+        }}
+      ></span>
+      <span
+        className={styles.star}
+        style={{
+          top: 0,
+          left: Math.floor(Math.random() * (100 - -100) + -100) + "%",
+          animationDelay: Math.random() * (0.8 - 0.2) + 0.2 + "s",
+          animationDuration: Math.floor(Math.random() * (3 - 2) + 2) + "s",
+        }}
+      ></span>
+      <span
+        className={styles.star}
+        style={{
+          top: 0,
+          left: Math.floor(Math.random() * (100 - -100) + -100) + "%",
+          animationDelay: Math.random() * (0.8 - 0.2) + 0.2 + "s",
+          animationDuration: Math.floor(Math.random() * (3 - 2) + 2) + "s",
+        }}
+      ></span>
+    </div>
+  );
+}
